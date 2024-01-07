6a4e42115aa20a3d3b402a0f2f88d0c1f4b7eb7d
shreyas-Github-code <mishrikotishreyas@gmail.com>
2024-01-07 15:48:29 +0530

    S commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+          Welcome to Jobster: Your Ultimate Job Tracking Companion. Streamline your work life effortlessly. Create, update, and track your job progress seamlessly. Stay on top of deadlines, manage tasks, and achieve your career milestones with ease. Experience the power of efficient job management, all in one intuitive app.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
